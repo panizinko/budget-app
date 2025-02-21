53b1c50db5322ea43251a0c20e136955a90d35b8
Adrian Zinko <zinko.adrian00@gmail.com>
2025-02-21 21:19:08 +0100

    chore: setup firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,52 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { initializeApp } from 'firebase/app';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import {
+  connectFirestoreEmulator,
+  Firestore,
+  getFirestore,
+  initializeFirestore,
+} from 'firebase/firestore';
+
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('AUTH', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('FIRESTORE', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
