a1fa780f88d3949b1d607f6f8b65f69267bfc8aa
Adrian Zinko <zinko.adrian00@gmail.com>
2025-02-21 08:56:55 +0100

    chore: setup tailwind and material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MatSlideToggleModule],
+  template: `
+    <h1 class="text-3xl font-bold underline p-4">  Hello, {{ title }}</h1>
+    <mat-slide-toggle>Toggle me!</mat-slide-toggle>
+    <router-outlet></router-outlet>
+  `,
+  styles: ''
 })
 export class AppComponent {
   title = 'budget-app';
